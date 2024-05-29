7550aff9284895e1b8bb2ba0765ed40c8b9fb377
JEESW <jeesw1221@gmail.com>
2024-05-29 16:53:10 +0900

    1st java lecture upload

---
+++ Java/1_240529/test.java
@@ -0,0 +1,16 @@
+public class test {
+
+    /**
+     * @author 굿
+     * @apiNote 이것은 무엇을 하는 기능입니다.
+     * @since 2021-07-01
+     * @deprecated
+     * @hidden
+     **/
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
