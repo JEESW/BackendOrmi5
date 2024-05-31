5c86c0c0f65fc68c9759f4158c98ae3ff97c4aa2
JEESW <jeesw1221@gmail.com>
2024-05-31 18:57:42 +0900

    3rd java lecture upload

---
+++ Java/3_240531/String1_240531.java
@@ -0,0 +1,86 @@
+public class String1_240531 {
+  public static void main(String[] args) {
+    //    String firstName = "seung woo";
+    //    String secondName = "Ji";
+    //    String phone = "Galaxy";
+    //    String status = "sad";
+
+    //    System.out.println("나는 " + secondName + " " + firstName + " ");
+    //    System.out.println(phone + " is " + status);
+
+    //    System.out.println(firstName.length());
+    //    System.out.println(firstName.toUpperCase());
+    //    System.out.println(firstName.equals(secondName));
+    //    System.out.println(firstName == secondName);
+
+    //    String a = "hello";
+    //    String b = new String("hello");
+    //
+    //    System.out.println(a.equals(b));  // true
+    //    System.out.println(a == b); // false
+
+    //    String c = "Hello World!";
+    //    System.out.println(c.indexOf("Hello")); // 문자열의 첫 문자가 있는 첫 인덱스를 반환함.
+    //    System.out.println(c.indexOf("g")); // 없는 값은 -1을 반환함.
+
+    //    String d = "ESTSOFT";
+    //    d.contains("SOFT");
+
+    //    String findChar = "What is your hobby";
+    //    System.out.println(findChar.charAt(5));
+    //    System.out.println("" + findChar.charAt(13) + findChar.charAt(14) + findChar.charAt(15) +
+    // findChar.charAt(16) + findChar.charAt(17));
+
+    //    String e = "Hello World! Hello World! Hello World!";
+    //    System.out.println(e.replace("World", "Java"));
+    //    System.out.println(e.replaceFirst("World", "Java"));
+    //    System.out.println(e.replaceAll("World", "Java"));
+
+    //    String f = "WEEEE ASKE ;ND";
+    //    System.out.println(f.substring(0, 3) + f.substring(8, 10) + f.substring(12, 14));
+
+    //    String g = "a:b:c:d";
+    //    String[] split = g.split(":");
+
+    //    String h = "Hello";
+    //    System.out.println(h.concat("!"));
+
+    //    String name1 = "Tim";
+    //    int age1 = 30;
+    //
+    //    String name2 = "Anna";
+    //    int age2 = 45;
+    //
+    //    String str1 = String.format("%s의 나이는 %s세 입니다.", name1, age1);
+    //    String str2 = String.format("%s의 나이는 %s세 입니다.", name2, age2);
+    //
+    //    System.out.println(str1);
+    //    System.out.println(str2);
+
+    //    String result = "";
+    //    result = "Hello ";
+    //    result += "Java ";
+    //    result += "World";
+    //    System.out.println(result);
+    //
+    //    StringBuilder sb = new StringBuilder();
+    //    sb.append("Hello ");
+    //    sb.append("Java ");
+    //    sb.append("World");
+    //    System.out.println(sb);
+    //
+    //    StringBuffer sf = new StringBuffer();
+    //    sf.append("Hello ");
+    //    sf.append("Java ");
+    //    sf.append("World");
+    //    System.out.println(sf);
+
+    //    StringBuffer sb = new StringBuffer();
+    //    sb.append("Hello");
+    //    sb.insert(1, "World");
+    //    sb.insert(1, "World");
+    //    sb.insert(1, "World");
+    //    sb.insert(1, "World");
+    //    System.out.println(sb);
+  }
+}
