c63b6a974df45f3be7a03f5935652b2b1bcf4f35
JEESW <jeesw1221@gmail.com>
2024-06-05 22:56:14 +0900

    6th java lecture upload

---
+++ Java/6_240605/Library_2_240605.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Library_2_240605 {
+  public static void main(String[] args) {
+    Random random = new Random();
+
+    //    int dice = random.nextInt(6) + 1;
+    //    System.out.println(dice);
+
+    //    ArrayList<Integer> list = new ArrayList<>();
+    //    ArrayList<Integer> arr = new ArrayList<>();
+    //
+    //    for (int i = 0; i < 45; i++) arr.add(i + 1);
+    //    for (int i = 0; i < 6; i++) {
+    //      int num = random.nextInt(45);
+    //      list.add(arr.get(num));
+    //      arr.remove(num);
+    //    }
+    //
+    //    list.sort(Integer::compareTo); // 오름차순 정렬
+    //    //    list.sort((a, b) -> b.compareTo(a));  // 내림차순 정렬
+    //
+    //    System.out.println("로또 번호: ");
+    //    for (int i : list) System.out.printf("[" + i + "] ");
+
+    //    Scanner sc = new Scanner(System.in);
+    //    Random rand = new Random();
+    //    int targetNumber = random.nextInt(100) + 1;
+    //    while (true) {
+    //      int input = sc.nextInt();
+    //      sc.nextLine();
+    //      if (input == targetNumber) {
+    //        System.out.println("정답입니다!");
+    //        break;
+    //      } else if (input > targetNumber) {
+    //        System.out.println("더 작습니다");
+    //      } else {
+    //        System.out.println("더 큽니다");
+    //      }
+    //    }
+    //    sc.close();
+
+    //    Scanner sc = new Scanner(System.in);
+    //
+    //    System.out.println("제목을 입력하세요.");
+    //    String theme = sc.nextLine();
+    //    System.out.println("관람 인원을 입력하세요.");
+    //    int personNum = sc.nextInt();
+    //    sc.nextLine();
+    //    System.out.println("예매자 이름을 입력하세요.");
+    //    String name = sc.nextLine();
+    //    System.out.println("전화 번호를 입력하세요.");
+    //    String phone = sc.nextLine();
+    //    int price = personNum * 10000;
+    //
+    //    System.out.println("===== 영화 티켓 예매 정보 =====");
+    //    System.out.println("영화 제목: " + theme);
+    //    System.out.println("관람 인원: " + personNum + "명");
+    //    System.out.println("예매자 이름: " + name);
+    //    System.out.println("예매자 전화번호: " + phone);
+    //    System.out.println("총 결제 금액: " + price + "원");
+    //
+    //    sc.close();
+  }
+}
---
+++ Java/6_240605/Scanner_1_240605.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Scanner_1_240605 {
+  public static void main(String[] args) {
+    //    Scanner sc = new Scanner(System.in);
+    //
+    //    System.out.println("정수를 입력하세요: ");
+    //    int a = sc.nextInt();
+    //
+    //    System.out.println("제가 입력한 값은 " + a + "입니다.");
+    //
+    //    sc.close();
+
+    //    Scanner sc = new Scanner(System.in);
+    //
+    //    System.out.println("정수를 입력하세요: ");
+    //    int number = sc.nextInt();
+    //    sc.nextLine(); // 정수형 변수에 개행("\n") 입력을 막기
+    //    System.out.println("문자열을 입력하세요: ");
+    //    String line = sc.nextLine();
+    //    System.out.println("정수는 = " + number);
+    //    System.out.println("문자열은 = " + line);
+    //
+    //    sc.close();
+
+    //    Scanner sc = new Scanner(System.in);
+    //    System.out.println("1~4 사이의 숫자를 입력하세요: ");
+    //    int n = sc.nextInt();
+    //    sc.nextLine();
+    //    switch (n) {
+    //      case 1:
+    //        System.out.println("봄");
+    //        break;
+    //      case 2:
+    //        System.out.println("여름");
+    //        break;
+    //      case 3:
+    //        System.out.println("가을");
+    //        break;
+    //      case 4:
+    //        System.out.println("겨울");
+    //        break;
+    //      default:
+    //        System.out.println("잘못된 입력입니다!");
+    //        break;
+    //    }
+    //
+    //    sc.close();
+  }
+}
